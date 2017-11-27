db386ef0f499784b87bbdeddaf76b19be316d58e
Ujang Arisandi <ujangaja@gmail.com>
2017-11-27 15:03:36 +0700

    membuat file komentar_ajax.php

---
+++ komentar_ajax.php
@@ -0,0 +1,16 @@
+<?php 
+	
+	// echo $_POST['isi_komentar']; ini untuk melihat di konsole
+include_once 'database.php';
+
+$komentar = mysqli_real_escape_string($link, $_POST['isi_komentar']);
+$query = "INSERT INTO komentar (isi_komentar, id_user) VALUES('$komentar', 1)";
+
+if(mysqli_query($link, $query)){
+	echo "true!";
+}else{
+	echo "Error!";
+}
+
+
+ ?>
