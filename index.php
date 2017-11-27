1054cb83156b86c0d941341726e49ea2068d2aa4
Ujang Arisandi <ujangaja@gmail.com>
2017-11-27 15:00:23 +0700

    membuat layout

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>komentar Ajax</title>
+	<script src="jquery-3.2.1.min.js"></script>
+
+	<style media="screen">
+		*{
+			font-family: 15px; font-family: sans-serif;
+		}
+		body{
+			width: 80%; margin: 10% auto;
+		}
+	</style>
+</head>
+<body>
+	<h1>Artikel Bukakelas.id</h1>
+	<p> Ini isi artikelnya</p>
+
+	<textarea name="textarea_komentar" id="textarea_komen" cols="40" rows="8"></textarea>
+	<input type="submit" name="subit_komen" id="subit_komen" value="submit">
+
+	<div id="komentar_wrapper">
+		
+	</div>
+
+
+</body>
+</html>
